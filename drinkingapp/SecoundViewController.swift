27bc97e68ca92f0bdb62afa225e4c4fb051e1119
jamei9824 <bernilippe@googlemail.com>
2017-08-28 21:02:29 +0200

    view controller

---
+++ drinkingapp/SecoundViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SecoundViewController.swift
+//  drinkingapp
+//
+//  Created by Bernhard Lippe on 28.08.17.
+//  Copyright © 2017 Bernhard Lippe. All rights reserved.
+//
+
+import UIKit
+
+class SecoundViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
