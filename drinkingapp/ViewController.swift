2d2fd0c9df7379a2252d36173a0d89e8e5ff9236
jamei9824 <bernilippe@googlemail.com>
2017-08-28 20:38:15 +0200

    Initial Commit

---
+++ drinkingapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  drinkingapp
+//
+//  Created by Bernhard Lippe on 28.08.17.
+//  Copyright © 2017 Bernhard Lippe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
